37e68569420e6d4b782cab7245cad96f3aa4a94b
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-03-17 15:26:37 +0100

    Tolto commento esercizio 1

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
 
   // Esercizio Parte 1
-  // var studente = {
-  //   'Nome': 'Alessandro',
-  //   'Cognome': 'Falcone',
-  //   'Età': '24'
-  // }
-  // for (var key in studente) {
-  //   console.log(key);
-  // }
+  var studente = {
+    'Nome': 'Alessandro',
+    'Cognome': 'Falcone',
+    'Età': '24'
+  }
+  for (var key in studente) {
+    console.log(key, studente[key]);
+  }
+
+  console.log("");
 
   // Esercizio Parte 2
   var amici = [
